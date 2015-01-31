4d417cb2e86288e9e4d4dd951886cb5cfaaab8ed
Andrew <quadcorder@gmail.com>
2015-01-31 15:26:36 -0500

    Added wborders

--- Assets/Scripts/WController.cs
+++ Assets/Scripts/WController.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class WController : MonoBehaviour {
 	public float minW;
 	public float maxW;
+	public float minWBorder;
+	public float maxWBorder;
 	public Vector3 minW_V;
 	public Vector3 maxW_V;
 	protected bool gone = false;
@@ -15,9 +17,15 @@ public class WController : MonoBehaviour {
 	public void PlayerUpdate(float wNow) {
 		Debug.Log ("SpinController PlayerUpdate");
 		Debug.Log (transform.position.GetType());
-		if (PlayerController.wNow <= maxW && PlayerController.wNow >= minW && !gone) {
+		if (PlayerController.wNow < maxW && PlayerController.wNow > minW && !gone) {
 			gameObject.SetActive (true);
-			transform.position = minW_V * (wNow-minW)/(maxW-minW) + maxW_V * (maxW-wNow)/(maxW-minW) ;
+			transform.position = maxW_V * (wNow-minW)/(maxW-minW) + minW_V * (maxW-wNow)/(maxW-minW);
+		} else if (PlayerController.wNow <= maxW+maxWBorder && PlayerController.wNow >= maxW) {
+			gameObject.SetActive (true);
+			transform.position = maxW_V;
+		} else if (PlayerController.wNow <= minW && PlayerController.wNow >= minW-minWBorder) {
+			gameObject.SetActive (true);
+			transform.position = minW_V;
 		} else {
 			gameObject.SetActive (false);
 		}
